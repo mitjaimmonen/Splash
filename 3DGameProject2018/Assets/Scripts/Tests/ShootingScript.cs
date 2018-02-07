17c8cb1ac8411a014204d03c3cf7b874a655487f
Mitja <mitja.immonen@hotmail.com>
2018-02-07 13:32:52 +0200

    Water gun model & animation testing.

---
+++ 3DGameProject2018/Assets/Scripts/Tests/ShootingScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+
+	private ParticleSystem m_ParticleSystem;
+	private Animator gunAnimator;
+
+
+	// Use this for initialization
+	void Awake () {
+		m_ParticleSystem = gameObject.GetComponentInChildren<ParticleSystem>();
+		gunAnimator = gameObject.GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			gunAnimator.SetBool("isShooting", true);
+			
+			m_ParticleSystem.Play();
+		} else if (Input.GetMouseButtonUp(0)) {
+			gunAnimator.SetBool("isShooting", false);
+			
+			m_ParticleSystem.Stop();
+
+		}
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			m_ParticleSystem.Play();
+		}
+	}
+}
