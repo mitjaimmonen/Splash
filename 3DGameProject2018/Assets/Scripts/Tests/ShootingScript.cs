c93a8d13f37c616d04d608b1f1d0399b3d0ecc36
Mitja <mitja.immonen@hotmail.com>
2018-02-08 11:43:09 +0200

    Fmod working with automation&parameters for one water gun.

--- 3DGameProject2018/Assets/Scripts/Tests/ShootingScript.cs
+++ 3DGameProject2018/Assets/Scripts/Tests/ShootingScript.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class ShootingScript : MonoBehaviour {
 
 	private ParticleSystem m_ParticleSystem;
+	private FMOD_StudioEventEmitter FMODEmitter;
+	private FMOD.Studio.ParameterInstance FMOD_Magazine;
+	private FMOD.Studio.ParameterInstance FMOD_Shooting;
+	private FMOD.Studio.EventInstance e;
+	private float magazine = 100f;
+		
+	[FMODUnity.EventRef] public string m_EventPath;
+
 	private Animator gunAnimator;
 
 
@@ -12,24 +20,49 @@ public class ShootingScript : MonoBehaviour {
 	void Awake () {
 		m_ParticleSystem = gameObject.GetComponentInChildren<ParticleSystem>();
 		gunAnimator = gameObject.GetComponentInChildren<Animator>();
+		FMODEmitter = gameObject.GetComponent<FMOD_StudioEventEmitter>();
+		e = FMODUnity.RuntimeManager.CreateInstance(m_EventPath);
+		e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+			e.getParameter("Magazine", out FMOD_Magazine);
+			e.getParameter("Shooting", out FMOD_Shooting);
+			
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (gunAnimator.GetBool("isShooting") && magazine > 0) {
+			Debug.Log(magazine);
+			magazine -= Time.deltaTime * 10;
+			FMOD_Magazine.setValue(magazine);
+		}
+			
+		
+
 		if (Input.GetMouseButtonDown(0)) {
 			gunAnimator.SetBool("isShooting", true);
+			magazine = 100f;
+			
+			FMOD_Shooting.setValue(1f);
+
+			e.start();
+			e.release();
 			
 			m_ParticleSystem.Play();
-		} else if (Input.GetMouseButtonUp(0)) {
+		} 
+		if (Input.GetMouseButtonUp(0) || magazine <= 0) {
 			gunAnimator.SetBool("isShooting", false);
-			
+				
+			FMOD_Shooting.setValue(0);
+			// e.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
 			m_ParticleSystem.Stop();
 
 		}
 
-		if (Input.GetMouseButtonDown(1))
-		{
-			m_ParticleSystem.Play();
-		}
+
 	}
+
+
+
 }
