3f9de2acdb6c47fe33b5a9cd12245df1b7838658
Mitja <mitja.immonen@hotmail.com>
2018-02-25 20:08:58 +0200

    Pseudo for Hud script

--- 3DGameProject2018/Assets/Scripts/Player Scripts/HudHandler.cs
+++ 3DGameProject2018/Assets/Scripts/Player Scripts/HudHandler.cs
@@ -4,6 +4,60 @@ using UnityEngine;
 
 public class HudHandler : MonoBehaviour {
 
-    //handle all the hud shit
-	
+    /*
+    Unclear??
+    - What class has health?
+    - Is hud attached to player?
+    - All weapons have own script, should hud get a list and check in update which is active?
+    - What script takes care of possible throwable items?
+    - How to get current player's camera?
+    
+     */
+
+    // Variables:
+    // Camera viewport size
+    // ext player controller & health
+    // ext weapon
+    // child Health bar
+    // child Health face icon
+    // child current ammo text
+    // child global ammo text
+    // (Maybe) child magazine size OR shot usage text
+    // (maybe) child "throwable" icon
+
+    public void Awake() {
+        //Get components
+
+        //If viewport scaling does not work well automatically:
+        //SetHudScaling(viewportSize)
+        
+    }
+
+    public void Update() {
+
+        //If changes in health:
+        Health();
+        //If changes in ammunition:
+        Ammo();
+        //If changes in throwable items:
+        Throwable();
+        
+    }
+
+    private void Health(){
+        //Update health bar
+        //Update health face icon
+    }
+
+    private void Ammo(){
+        //Update global ammo
+        //Update current weapon ammo
+        //Update ammo bar
+
+    }
+
+    private void Throwable() {
+        //Update if throwable available
+    }
+
 }
